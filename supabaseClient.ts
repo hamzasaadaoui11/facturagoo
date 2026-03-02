4fb683dbcfd9a63c73a58b13c2c2f85b08cedd1e
hamzasaadaoui11 <hamzasaadaoui.hs@gmail.com>
2026-03-02 15:28:41 +0000

    Add files via upload

---
+++ supabaseClient.ts
@@ -0,0 +1,17 @@
+
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://vjcfqgzbzskfihtkanza.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_x_iYgckRVzAjsrR9oAreoA_wz11aNiz';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    storage: window.localStorage
+  },
+  global: {
+    fetch: globalThis.fetch?.bind(globalThis)
+  }
+});
